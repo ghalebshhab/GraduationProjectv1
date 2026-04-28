2dca467a694ac11d6212e33bcefc98d29e3a1dd5
ziadmq <kafehazyad5@gmail.com>
2026-04-28 21:36:21 +0300

    connect with DB

--- src/main/java/com/jomap/backend/Entities/Users/Profile/UserProfile.java
+++ src/main/java/com/jomap/backend/Entities/Users/Profile/UserProfile.java
@@ -1,10 +1,19 @@
 package com.jomap.backend.Entities.Users.Profile;
 
-import com.jomap.backend.Entities.Users.User;
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.time.LocalDate;
+
+import com.jomap.backend.Entities.Users.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "user_profiles")
@@ -22,7 +31,7 @@ public class UserProfile {
     @Column(length = 500)
     private String bio;
 
-    @Column(name = "cover_image_url", columnDefinition = "LONGTEXT")
+    @Column(name = "cover_image_url", columnDefinition = "TEXT")
     private String coverImageUrl;
 
     @Column(length = 150)
