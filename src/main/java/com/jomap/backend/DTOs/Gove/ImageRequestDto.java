c03798197200e7157829b74866bd71ba676620b6
Abedalqader Froukh <abdalqaderfroukh1@gmail.com>
2026-05-04 19:25:52 +0300

    add Dtos

---
+++ src/main/java/com/jomap/backend/DTOs/Gove/ImageRequestDto.java
@@ -0,0 +1,7 @@
+package com.jomap.backend.DTOs.Gove;
+
+import lombok.Data;
+@Data
+public class ImageRequestDto {
+    private String imageUrl;
+}
---
+++ src/main/java/com/jomap/backend/DTOs/Gove/PlaceRequestDto.java
@@ -0,0 +1,10 @@
+package com.jomap.backend.DTOs.Gove;
+
+import lombok.Data;
+
+@Data
+public class PlaceRequestDto {
+    private String name;
+    private String description;
+    private String imageUrl;
+}
