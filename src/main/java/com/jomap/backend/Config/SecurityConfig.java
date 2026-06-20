2973313540dc0745b8504c7607832f9774cdf746
ziadmq <kafehazyad5@gmail.com>
2026-06-20 09:03:00 +0300

    chore: Update Google Client ID and add live API URL to CORS

--- src/main/java/com/jomap/backend/Config/SecurityConfig.java
+++ src/main/java/com/jomap/backend/Config/SecurityConfig.java
@@ -74,7 +74,11 @@ public class SecurityConfig {
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
 
-                configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+                configuration.setAllowedOrigins(List.of(
+                        "http://localhost:5173",
+                        "http://localhost:3000",
+                        "https://jomab-712232187160.europe-west1.run.app"
+                ));
                 configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("*"));
                 configuration.setAllowCredentials(true);
